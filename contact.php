11300522b545d5fce52ca5cea79707dd91772b47
Sabina <sabina@ischool.berkeley.edu>
2016-04-24 22:31:59 -0700

    added
    added

---
+++ contact.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+ 
+    <title>Contact</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap-3.3.4-dist/css/starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+<body>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">FVS</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.html">Home</a></li>   <!-- class="active"-->
+                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Fruits<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                <li><a href="apple.html">Apple</a></li>
+                <li><a href="avocado.html">Avocado</a></li>
+                <li><a href="#">Banana</a></li>
+                <li><a href="#">Cantaloupe</a></li>
+                <li><a href="#">Grapefruit</a></li>
+                <li><a href="#">Grapes</a></li>
+                <li><a href="#">Honeydew Melon</a></li>
+                <li><a href="#">Kiwifruit</a></li>
+                <li><a href="#">Lemon</a></li>
+                <li><a href="#">Lime</a></li>
+                <li><a href="#">Nectarine</a></li>
+                <li><a href="#">Orange</a></li>
+                <li><a href="#">Peach</a></li>
+                <li><a href="#">Pineapple</a></li>
+                <li><a href="#">Plums</a></li>
+                <li><a href="#">Strawberries</a></li>
+                <li><a href="#">Cherries</a></li>
+                <li><a href="#">Tangerine</a></li>
+                <li><a href="#">Watermelon</a></li>
+                </ul>
+                </li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Vegetables<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+            	<li><a href="asparagus.html">Asparagus</a></li>
+                <li><a href="bellpepper.html">Bell Pepper</a></li>
+                <li><a href="#">Broccoli</a></li>
+                <li><a href="#">Carrot</a></li>
+                <li><a href="#">Cauliflower</a></li>
+                <li><a href="#">Celery</a></li>
+                <li><a href="#">Cucumber Melon</a></li>
+                <li><a href="#">Green Beans</a></li>
+                <li><a href="#">Green Cabbage</a></li>
+                <li><a href="#">Green Onion</a></li>
+                <li><a href="#">Iceberg Lettuce</a></li>
+                <li><a href="#">Leaf Lettuce</a></li>
+                <li><a href="#">Mushrooms</a></li>
+                <li><a href="#">Onion</a></li>
+                <li><a href="#">Potato</a></li>
+                <li><a href="#">Radishes</a></li>
+                <li><a href="#">Summer Squash</a></li>
+                <li><a href="#">Sweet Corn</a></li>
+                <li><a href="#">Sweet Potato</a></li>
+                <li><a href="#">Tomato</a></li>
+            </ul>
+            </li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Salads<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+             <li><a href="azerbaijani.html">Azerbaijani</a></li>
+            <li><a href="italian.html">Italian</a></li>
+            <li><a href="#">Turkish</a></li>
+            <li><a href="#">Chinese</a></li>
+            <li><a href="#">Russian</a></li>
+            <li><a href="#">French</a></li>
+            <li><a href="#">Spanish</a></li>
+            <li><a href="#">Japanese</a></li>
+            <li><a href="#">Indian</a></li>
+            <li><a href="#">Greek</a></li>
+            <li><a href="#">Thai</a></li>
+            <li><a href="#">Mexican</a></li>
+            </ul>
+            </li>
+                <li><a href="article.html">Articles</a></li>
+                <li class="active"><a href="contact.html">Contact</a></li>
+               <!-- <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>-->
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    
+    <div class="container">
+
+      <div class="starter-template">
+      <br />
+     
+      <fieldset>
+      
+      
+      <?php
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+
+  $name = $_POST['firstname'];
+  $email = $_POST['email'];
+  $contact = $_POST['contact'];
+
+  $to = 'sshahbazzade@gmail.com';
+  $subject = 'FVS';
+  // Additional headers
+$headers .= 'To: Sabina <sshahbazzade@gmail.com>' . "\r\n";
+$from .= 'From: FVS <fvs@fvs.com>' . "\r\n";
+  $msg = "Name: $name\n" .
+    	 "Email: $email\n" .
+   		 "Text: $contact";
+  mail($to, $subject, $msg, $from, $email);
+
+  echo 'Thanks for sending message to us!<br />';
+ 
+  
+?>
+
+	</fieldset>
+      </div>
+     
+ <!-- FOOTER -->
+      <div class="navbar navbar-fixed-bottom" role="navigation">  
+ <div class="container">
+ <hr />
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2015 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </div>
+     </div>
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script src="bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+
+</body>
+</html>
